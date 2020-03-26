fcb4c6ac3a728ffa4a5c5649fe01eb4c83da07b4
Qiang Li <liqiang1205@gmail.com>
2020-03-26 12:00:58 -0700

    Add files via upload

---
+++ Apportionment_Peace_Hunt.R
@@ -0,0 +1,104 @@
+require(dplyr)
+require(sf)
+require(streamDepletr)
+require(sp)
+library(lubridate)
+library(magrittr)
+
+yrs.model <- c(1:35) 
+weeks.model<-c(1:52)
+DOYs.all<- rep(weeks.model*7, times = length(yrs.model)) + rep(52*(yrs.model-1)*7, each = length(weeks.model))
+
+df.all<-read.csv("well_stream_geometry.csv")
+#head(df.all)
+df.all$segment = paste(df.all$seg, df.all$seg_num, sep= "_")
+
+df.trans <- read.csv("well_T.csv")
+head(df.trans)
+df.trans <- df.trans[df.trans$Trans>0, ]
+
+df.cond <- read.csv("well_stream_cond.csv")
+head(df.cond)
+df.cond$segment = paste(df.cond$seg, df.cond$seg_num, sep= "_")
+df.all <- unique(df.all)
+
+min_frac <- 0.01  # minimum depletion considered
+w.start.flag <- T
+counter <- 0
+
+df.all <- subset(df.all, well_num %in% df.trans$well_num)
+df.all<-unique(df.all)
+
+for (w in unique(df.all$well_num)){
+  dist_w_pts <- subset(df.all, well_num==w)
+  dist_w_pts <- dist_w_pts[!((dist_w_pts$well_row == dist_w_pts$seg_row) & (dist_w_pts$well_col == dist_w_pts$seg_col)), ]
+  
+  # closest point only
+  dist_w_pts_closest <- 
+    dist_w_pts %>% 
+    group_by(segment) %>% 
+    filter(distance == min(distance))
+  
+  # first: Theissen polygons to figure out adjacent catchments
+  df.apportion.t <- 
+    dist_w_pts_closest %>% 
+    apportion_polygon(., 
+                      wel_lon = dist_w_pts_closest$well_col[1],  # column
+                      wel_lat = dist_w_pts_closest$well_row[1],  # row
+                      crs = CRS(st_crs("+init=epsg:32610")[["proj4string"]]),  # any projected CRS (BC UTM?)
+                      reach_name = "segment",
+                      dist_name = "distance",
+                      lon_name = "seg_col",
+                      lat_name = "seg_row",
+                      min_frac = min_frac) %>% 
+    set_colnames(c("segment", "frac_depletion"))
+  
+  #expanding 
+  for (time_days in DOYs.all){
+    
+    # find maximum distance, based on maximum observed S, Tr, lmda (inclusive estimate)
+    max_dist <- depletion_max_distance(Qf_thres = min_frac,
+                                       d_interval = 250,
+                                       d_min = 500,
+                                       d_max = max(dist_w_pts$distance),
+                                       method="hunt",  # hunt
+                                       t = time_days,
+                                       S = df.trans$S[df.trans$well_num==w],
+                                       Tr = df.trans$Trans[df.trans$well_num==w],
+                                       lmda = max(df.cond$cond),
+                                       lmda_max = 1
+                                       )
+    
+    # second: use web^2 to apportion to any stream segment that is within max_dist
+    #         OR that is adjacent to well based on Theissen Polygon
+    df.apportion.w.t <- 
+      dist_w_pts %>% 
+      subset((distance <= max_dist) | (segment %in% df.apportion.t$segment)) %>% 
+      apportion_web(reach_dist = ., 
+                    w = 2, 
+                    min_frac = min_frac,
+                    reach_name = "segment",
+                    dist_name = "distance") %>% 
+      set_colnames(c("segment", "frac_depletion")) %>% 
+      transform(well_num = w,
+                time_days = time_days) %>% 
+      left_join(dist_w_pts_closest, by=c("segment", "well_num"))
+    
+    if (w.start.flag){
+      df.out <- df.apportion.w.t[ , !(names(df.apportion.w.t) %in% c("lon", "lat", "SegNum_GrowNum"))]
+      w.start.flag <- F
+    } else {
+      df.out <- rbind(df.out, df.apportion.w.t[ , !(names(df.apportion.w.t) %in% c("lon", "lat", "SegNum_GrowNum"))])
+    }
+    
+    # update max_dist starting value
+    max_dist_prev <- max_dist
+    
+  }
+  
+  # status update
+  counter <- counter+1
+  print(paste0("Well ", counter, " of ", length(unique(df.all$well_num)), " depletion apportionment complete, ", Sys.time()))
+}
+
+write.csv(df.out, "Hunt_Apportionment.csv", row.names = F)
---
+++ Peace_Glover_Model.R
@@ -0,0 +1,120 @@
+require(dplyr)
+require(sf)
+require(streamDepletr)
+require(sp)
+library(lubridate)
+library(magrittr)
+library(zoo)
+library(plyr)
+library(dplyr)
+library(rsq)
+library(ggplot2)
+library(gtable)
+library(grDevices)
+library(graphics)
+library(gridExtra)
+library(hydroGOF)
+library(magrittr)
+
+## load depletion apportionment output
+df.out <- read.csv("Glover_apportionment_ss1.csv", stringsAsFactors=F)
+df.trans <- read.csv("well_T.csv")
+df.trans <- df.trans[df.trans$Trans>0, ]
+
+head(df.trans)
+
+### what dates do you want output for? (units: number of days since start of pumping)
+# convert years and dates to DOY since pumping started
+yrs.model <- c(1:35) 
+weeks.model<-c(1:52)
+DOYs.all<- rep(weeks.model*7, times = length(yrs.model)) + rep(52*(yrs.model-1)*7, each = length(weeks.model))
+
+### load and pre-process data
+df.pump <- data.frame(MonthNum = c(1:18, 19:22, 23:26, 27:30, 31:34, 35:38, 39:52),
+                      Qw_m3day = c(rep(0, each = length(1:18)), 
+                                   rep(19, each = length(19:22)),
+                                   rep(108, each = length(23:26)),
+                                   rep(214, each = length(27:30)),
+                                   rep(171, each = length(31:34)),
+                                   rep(84, each = length(35:38)),
+                                   rep(0, each = length(39:52))))
+
+df.pump$MonthLengthDays <- 7
+# set up long data frames for intermittent_pumping script; separate for outdoor and greenhouse
+t.max.yrs <- max(yrs.model)
+
+df.pump.long <- 
+  df.pump %>% 
+  replicate(t.max.yrs, ., simplify = FALSE) %>% 
+  dplyr::bind_rows() %>% 
+  transform(EndOfMonthDays = cumsum(MonthLengthDays))
+df.pump.long$StartOfMonthDays <- c(1, df.pump.long$EndOfMonthDays[1:(t.max.yrs*52)-1]+1)
+
+# combine identical periods in df.pump.long
+i.starts <- which(c(1, diff(df.pump.long$Qw_m3day)) != 0)
+i.ends <- c((i.starts-1)[2:length(i.starts)], i.starts[length(i.starts)])
+
+df.pump.compressed <-
+  data.frame(StartOfMonthDays = df.pump.long$StartOfMonthDays[i.starts],
+             EndOfMonthDays = df.pump.long$EndOfMonthDays[i.ends],
+             Qw_m3day = df.pump.long$Qw_m3day[i.ends])
+
+## unique well-seg combos
+df.combos <- 
+  df.out %>% 
+  dplyr::select(segment, well_num, distance) %>% 
+  unique()
+
+df.combos <- 
+  left_join(df.combos, df.trans, by="well_num")%>%
+  unique()
+
+## calculate depletion for all combos
+start.flag.Qs <- T
+for (i in 1:dim(df.combos)[1]){
+  # identify well-seg combo
+  seg <- df.combos$segment[i]
+  well <- df.combos$well_num[i]
+  
+  # get times
+  output_t_days <- df.out$time_days[df.out$segment==seg & df.out$well_num==well]
+  output_frac <- df.out$frac_depletion[df.out$segment==seg & df.out$well_num==well]
+  
+  # calculate depletion
+  Qs <- intermittent_pumping(t = output_t_days,
+                             starts = df.pump.compressed$StartOfMonthDays,
+                             stops  = df.pump.compressed$EndOfMonthDays,
+                             rates  = df.pump.compressed$Qw_m3day,
+                             method = "glover",
+                             d = df.combos$distance[i],
+                             S = df.combos$S[i],
+                             Tr = df.combos$Trans[i]
+                             )
+  
+  
+  # compile output
+  df.depletion <- data.frame(segment = seg,
+                             well_num = well,
+                             time_days = output_t_days,
+                             Qs = Qs)
+  
+  if (start.flag.Qs){
+    df.Qs <- df.depletion
+    start.flag.Qs <- F
+  } else {
+    df.Qs <- rbind(df.Qs, df.depletion)
+  }
+  
+  print(paste0("Depletion ", i, " of ", dim(df.combos)[1], " complete, ", Sys.time()))
+  
+}
+
+# combine and save
+df.final <- 
+  df.Qs %>% 
+  left_join(df.out, by=c("segment", "well_num", "time_days")) %>% 
+  transform(depletion_m3d = Qs*frac_depletion) %>% 
+  dplyr::select(segment, well_num, time_days, Qs, frac_depletion, depletion_m3d) %>% 
+  subset(depletion_m3d >= 1e-3) %>% 
+  unique()%>% 
+  write.csv("Peace_Glover_ss1.csv", row.names=F)
